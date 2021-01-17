266d7f5ac1f7548a5e2a8ffc6fe072146afee120
Andy <76767534+andyian98@users.noreply.github.com>
2021-01-17 16:48:49 -0300

    Add files via upload

---
+++ ejercicio funciones.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+//Prototipo de función
+
+int encontrarmax (int x, int y);
+
+int main(){
+	
+	int n1=0, n2=0;
+	int cualnum;
+	cout<<"Digite dos numeros please"<<endl;
+	cin>>n1;
+	cin>>n2;
+	
+	cualnum<-encontrarmax(n1,n2);
+	cout<<"El numero mayor entre los digitados es: "<<cualnum;
+	
+	getch();
+	return 0;
+}
+
+
+//Definición de función
+int encontrarmax (int n1, int n2){
+	int numax=0;
+	if (n1>n2){
+		numax=n1;
+	}else{
+		numax=n2;
+	}
+	return numax;	
+} 
